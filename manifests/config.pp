b0a703be87e4725cc238ddbe14981172a8e08818
David Goodlad <david@goodlad.net>
2013-02-25 09:26:04 +1100

    Add a basic s3cmd config template and defined type

---
+++ manifests/config.pp
@@ -0,0 +1,15 @@
+define s3cmd::config (
+  $aws_access_key_id,
+  $aws_secret_access_key,
+  $owner,
+  $group,
+) {
+  $path = $name
+
+  file { $path:
+    content => template('s3cmd/s3cfg.erb'),
+    owner   => $owner,
+    group   => $group,
+    mode    => '0400',
+  }
+}
